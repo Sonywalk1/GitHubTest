0cf30be9665f2b427c3820c9679af72a8e90fa54
Sonywalk1 <marluv211@studentmail.oru.se>
2021-05-11 10:30:36 +0200

    Ändrat lite och tagit bort ett projekt

--- JavaApplication4/src/GitHubTest/GitHubTest.java
+++ JavaApplication4/src/GitHubTest/GitHubTest.java
@@ -10,6 +10,10 @@ package GitHubTest;
  */
 public class GitHubTest {
     
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         
         System.out.println("Hello world!");
